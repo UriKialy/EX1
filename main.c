2f7657d6dabf2d564dd90c62331c33371ccd28e8
urikialy <uri.kialy@msmail.ariel.ac.il>
2024-01-23 22:47:20 +0200

    update

--- main.c
+++ main.c
@@ -10,43 +10,43 @@ int main()
     scanf("%d", &first);
     scanf("%d", &last);
     printf("\n");
-    printf("The Armstrong numbers are: ");
+    printf("The Armstrong numbers are:");
     a = first, b = last;
     for (; a <= b; a++)
     {
         if (isArmstrong(a) == 1)
         {
-            printf("%d ", a);
+            printf(" %d", a);
         }
     }
     printf("\n");
-    printf("The Palindromes are: ");
+    printf("The Palindromes are:");
     a = first, b = last;
     for (; a <= b; a++)
     {
         if (isPalindrome(a) == 1)
         {
-            printf("%d ", a);
+            printf(" %d", a);
         }
     }
     printf("\n");
-    printf("The Prime numbers are: ");
+    printf("The Prime numbers are:");
     a = first, b = last;
     for (; a <= b; a++)
     {
         if (isPrime(a) == 1)
         {
-            printf("%d ", a);
+            printf(" %d", a);
         }
     }
     printf("\n");
-    printf("The Strong numbers are: ");
+    printf("The Strong numbers are:");
     a = first, b = last;
     for (; a <= b; a++)
     {
         if (isStrong(a) == 1)
         {
-            printf("%d ", a);
+            printf(" %d", a);
         }
     }
     printf("\n");
